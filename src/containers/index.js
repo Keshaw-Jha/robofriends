eddfe5c0aa186835154b696197b1b62f182c0abe
Keshaw-Jha <78222085+Keshaw-Jha@users.noreply.github.com>
2023-05-03 19:50:59 +0530

    new arranged folders

--- src/containers/index.js
+++ src/containers/index.js
@@ -4,16 +4,9 @@ import './index.css';
 import App from './App';
 import 'tachyons';
 
-
-// import registerServiceWorker from './registerServiceWorker';
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
     <App/>
   </div>
 );
-
-// registerServiceWorker();
